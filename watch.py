8cd67fb48a77c8a6f0f3a2cd2b35b24357a1cd50
Gareth Murphy <garethcmurphy@gmail.com>
2019-04-13 06:31:00 +0200

    add remote copy

--- watch.py
+++ watch.py
@@ -5,17 +5,36 @@ import logging
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 from watchdog.events import FileSystemEventHandler
-
+import paramiko
+from paramiko import SSHClient
+from scp import SCPClient
+import json
 
 class MyEventHandler(FileSystemEventHandler):
-    pass
+    def __init__(self):
+        pass
+
+    def on_created(self, event):
+        configfile="../config.json"
+        if (configfile):
+            with open(configfile, 'r') as f:
+                config = json.load(f)
+        ssh = SSHClient()
+        k = paramiko.Ed25519Key.from_private_key_file(config["privatekey"])
+        ssh.load_system_host_keys()
+        ssh.connect(hostname=config["hostname"],username=config["username"],pkey=k)
+        scp = SCPClient(ssh.get_transport())
+        scp.put(event.src_path)
+
+        print("e=", event)
+        
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
+    event_handler = MyEventHandler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
